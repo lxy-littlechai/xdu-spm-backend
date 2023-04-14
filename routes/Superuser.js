0791a85b5af58ba3b36a638576ff595eea0a75d9
lxy-littlechai <76553203+lxy-littlechai@users.noreply.github.com>
2023-04-14 23:21:18 +0800

    Update Superuser.js

--- routes/Superuser.js
+++ routes/Superuser.js
@@ -4,6 +4,7 @@ var router = express.Router();
 // 引入数据库配置文件
 const db = require('./database');
 
+//查日志
 router.post('/Log', (req, res) => {
   let sql = `select * from log `
   db.query(sql, (err, result) => {
@@ -17,6 +18,33 @@ router.post('/Log', (req, res) => {
   })
 })
 
+//查所有借的书
+router.post('/GetAllborrowedBook', (req, res) => {
+  let sql = `select * from borrowedbook `
+  db.query(sql, (err, result) => {
+    if (err) {
+      return;
+    }
+    res.json({
+      status: "200",
+      result
+    })
+  })
+})
+
+//查所有用户记录
+router.post('/GetAllUser', (req, res) => {
+  let sql = `select * from user `
+  db.query(sql, (err, result) => {
+    if (err) {
+      return;
+    }
+    res.json({
+      status: "200",
+      result
+    })
+  })
+})
 
 
 
